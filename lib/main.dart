7d0382c1ca75cfafca1fc035d70b85fe1d7c8cbb
jinoshprabhuraj5 <jinoshprabhuraj5@gmail.com>
2022-05-26 10:23:53 +0530

    Refresh and Insert Functions are added

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,108 @@
 import 'package:flutter/material.dart';
+import 'package:sqflite_example/services/database_helper.dart';
 
-void main() => runApp(MyApp());
+void main(List<String> args) {
+  runApp(const MaterialApp(
+    home: MyApp(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  DatabaseHelper databaseHelper = DatabaseHelper.instance;
+  List<Map<String, dynamic>> allDatas = [];
+  final TextEditingController _name = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    getDataBase;
+  }
+
+  getDataBase() async {
+    allDatas = await databaseHelper.queryAll();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: const Text('Hello World'),
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sqflite Example'),
+        actions: [
+          IconButton(
+              onPressed: () => getDataBase, icon: const Icon(Icons.refresh))
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.6,
+                  child: TextFormField(
+                      controller: _name,
+                      style: const TextStyle(
+                        fontSize: 16,
+                      ),
+                      decoration: const InputDecoration.collapsed(
+                        hintText: 'Enter Name: ',
+                      )),
+                ),
+                ElevatedButton(
+                    onPressed: () async {
+                      int rowId = await databaseHelper.insert({
+                        DatabaseHelper.columnName: _name,
+                      });
+                      debugPrint(rowId.toString());
+                      _name.clear();
+                      getDataBase();
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.pink,
+                    ),
+                    child: const Text(
+                      'Insert',
+                      style: TextStyle(
+                        fontSize: 14,
+                      ),
+                    ))
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Text(
+              'Datas :',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+              ),
+            ),
+            //
+            allDatas.isNotEmpty
+                ? ListView.builder(
+                    itemCount: allDatas.length,
+                    shrinkWrap: true,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Text(
+                        'Id ${allDatas[index][DatabaseHelper.columnName]}',
+                      );
+                    },
+                  )
+                : const Text('No Datas Found')
+          ],
         ),
       ),
     );
