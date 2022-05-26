f511532512bf259128e4870f621c9097f5f93913
jinoshprabhuraj5 <jinoshprabhuraj5@gmail.com>
2022-05-26 10:20:46 +0530

    Project Initialised and Database Helper Created

---
+++ lib/services/database_helper.dart
@@ -0,0 +1,66 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DatabaseHelper {
+  static const _dbName = 'my_sql_database.db';
+  static const _dbVersion = 1;
+
+  static const _tableName = 'my_table';
+  static const columnId = '_id';
+  static const columnName = 'name';
+
+  DatabaseHelper._privateConstructor();
+
+  static final instance = DatabaseHelper._privateConstructor();
+
+  static Database? _database;
+
+  get database async {
+    if (_database == null) {
+      _database = await initialiseDatabase();
+    } else {
+      return _database;
+    }
+  }
+
+  Future<Database> initialiseDatabase() async {
+    Directory directory = await getApplicationDocumentsDirectory();
+    String path = join(directory.path, _dbName);
+    debugPrint(path);
+    return await openDatabase(path, version: _dbVersion, onCreate: _onCreate);
+  }
+
+  Future _onCreate(Database db, int version) async {
+    db.execute('''
+CREATE TABLE $_tableName (
+$columnId INTEGER PRIMARY KEY,
+$columnName TEXT NOT NULL )
+''');
+  }
+
+  Future<int> insert(Map<String, dynamic> row) async {
+    Database db = await instance.database;
+    return await db.insert(_tableName, row);
+  }
+
+  Future<List<Map<String, dynamic>>> queryAll() async {
+    Database db = await instance.database;
+    return await db.query(_tableName);
+  }
+
+  Future<int> delete(int id) async {
+    Database db = await instance.database;
+    return await db.delete(_tableName, where: '$columnId = ?', whereArgs: [id]);
+  }
+
+  Future<int> update(Map<String, dynamic> row) async {
+    Database db = await instance.database;
+    int id = row[columnId];
+    return await db
+        .update(_tableName, row, where: '$columnId = ?', whereArgs: [id]);
+  }
+}
